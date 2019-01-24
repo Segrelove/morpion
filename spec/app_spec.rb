acf5a8ea7ac902ba621ada0f5d0437c506aa122e
Segrelove <mlesegret@gmail.com>
2019-01-24 09:44:24 +0100

    Structure

---
+++ spec/app_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/app.rb'
+# Free Synthaxe for ya
+describe 'scrap coin market should return someting' do 
+    it 'should return a hash' do 
+        expect(your_method).to be_truthy
+    end
+end
