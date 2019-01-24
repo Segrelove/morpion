33803e3128e65da17cd66826b2f76a747f30850f
Greg <38353404+GREGLEPIC@users.noreply.github.com>
2019-01-25 00:47:19 +0100

    Update board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,9 @@ require 'pry'
 require 'game'
 require 'application'
 
+# The board class is a model that care about our table game. 
+# The class initialize the table game and thabks to the player choice will update the table game round after round.
+
 class Board
   attr_accessor :case_name
 
@@ -14,6 +17,7 @@ class Board
     @cases = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
+  # This def ask one player after one what they want to play.
   def choice
     #
     # JOUEUR 1
@@ -22,15 +26,15 @@ class Board
     puts "Quel choix souhaites-tu, joueur 1?"
     puts "Tes choix : #{ouech}"
     @choice1 = gets.chomp.to_i
-    @result1 = @choice1 - 1
-    @@winner_x << @choice1
-    @cases[@result1] = "xXx"
+    @result1 = @choice1 - 1     # We store the index of the case the player choose
+    @@winner_x << @choice1      # We store all the case the player pick up to check later if he had one of the eight winning combination inside his array
+    @cases[@result1] = "xXx"    # We change the value of our @case index to display a new table with the choice of the player !   
     @cases.map { |x| x == @result1 ? 'xXx' : x }
     @rows[@result1] = "xXx"
-    if check_if_won(@@winner_x)
+    if check_if_won(@@winner_x)     # Thanks to the winning array we are chacking if the player win
       puts "Joueur 1 a gagné !"
       display_board
-      Application.new.perform
+      Application.new.perform       # if the game is finish because he win, we start back to the router to ask if the player want to start a new game
     end 
     #
     # JOUEUR 2
@@ -53,6 +57,8 @@ class Board
     
   end
 
+  # this def is use to start a table. Then when a player choose a case, it will automaticly udpate thanks to the case[index]
+  
   def update_board 
     @rows = []
     @rows << [@cases[0], @cases[1], @cases[2]]
@@ -66,6 +72,8 @@ class Board
       update_board
     end
   end
+  
+  # the winning def checking. We list all the eight possible combination into an array and we compare it with the array of the player choice to see if their is a match ! 
   
   def check_if_won(round)
     # rounds est l'array des tours joués par les deux joueurs
